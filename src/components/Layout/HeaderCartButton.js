9011ee821dd169215f6e4dc4ba9604cf91b52bcc
Carolina Maffia <carolinamaffia.m@gmail.com>
2021-07-16 12:33:42 -0300

    Modal cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
-    return <button>
-        <span>
-            
+    return <button className={styles.button} onClick={props.onClick}>
+        <span className={styles.icon}>
+            <CartIcon />
         </span>
-        <span></span>
-        <span></span>
+        <span>Your Cart</span>
+        <span className={styles.badge}>
+            3
+        </span>
     </button>
 };
 
